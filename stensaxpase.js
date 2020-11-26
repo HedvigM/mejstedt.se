7cb1513254c9a2070675274b9f746994cd24aa78
HedvigM <hedvig@mejstedt.se>
2020-11-26 08:44:51 +0100

    sten sax påse

--- stensaxpase.js
+++ stensaxpase.js
@@ -1,4 +1,4 @@
-const getUserChoise = userInput => {
+const getUserChoice = userInput => {
     userInput = userInput.toLowerCase();
 
     if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb') {
@@ -8,7 +8,7 @@ const getUserChoise = userInput => {
     }
 };
 
-const getComputerChoise = () => {
+const getComputerChoice = () => {
     randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
         case 0:
@@ -21,45 +21,87 @@ const getComputerChoise = () => {
 };
 
 
-const determineWinner = (userChoise, computerChoise) => {
-    if (userChoise === computerChoise) {
+const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice) {
         return 'The game was a tie!';
     }
 
-    if (userChoise === 'rock') {
-        if (computerChoise === 'paper') {
+    if (userChoice === 'rock') {
+        if (computerChoice === 'paper') {
             return 'The computer won!';
         } else {
             return 'The user won!';
         }
     }
 
-    if (userChoise === 'scissors') {
-        if (computerChoise === 'rock') {
+    if (userChoice === 'scissors') {
+        if (computerChoice === 'rock') {
             return 'The computer won!';
         } else {
             return 'The user won!';
         }
     }
 
-    if (userChoise === 'paper') {
-        if (computerChoise === 'scissors') {
+    if (userChoice === 'paper') {
+        if (computerChoice === 'scissors') {
             return 'The computer won';
         } else {
             return 'The user won!';
         }
     }
 
-    if (userChoise === 'bomb') {
+    if (userChoice === 'bomb') {
         return 'You won!';
     }
 };
 
-const playGame = () => {
-    const userChoise = getUserChoise('bomb');
-    const computerChoise = getComputerChoise();
-    console.log('You threw: ' + userChoise);
-    console.log('The computer threw: ' + computerChoise);
-    console.log(determineWinner(userChoise, computerChoise));
+const answerElement = document.getElementById("answer")
+
+const playGame = (choice) => {
+    const userChoice = getUserChoice(choice);
+    const computerChoice = getComputerChoice();
+    console.log('You threw: ' + userChoice);
+    console.log('The computer threw: ' + computerChoice);
+    console.log(determineWinner(userChoice, computerChoice));
+    answerElement.innerText = `
+        Du valde ${userChoice}\n
+        Datorn valde ${computerChoice}\n
+        ${determineWinner(userChoice, computerChoice)}
+    `
 };
-playGame();
+
+const handleClick = (event) => {
+    const choice = event.target.value;
+    playGame(choice);
+};
+
+const rockButton = document.getElementById("rock")
+rockButton.addEventListener('click', handleClick);
+
+const scissorsButton = document.getElementById("scissors")
+scissorsButton.addEventListener('click', handleClick);
+
+const paperButton = document.getElementById("paper")
+paperButton.addEventListener('click', handleClick);
+
+
+
+
+
+
+/*
+1. gör en knapp som anropar en funktion och visar nått i konsollen.
+* inputfält (<input />) av typen text
+* knapp (<button>) med en onclick-metod
+* text-element (<div><p>)
+* onclickmetoden:
+    * när man trycker på knappen, läs värdet i input-fältet (getelementbyid)
+    * anropa getUserChoic med det värdet
+    * anropa computerchoice
+    * anropa determineWinner
+    * sätt värdet på textelementet till resultatet
+
+
+p tagg
+
+*/
